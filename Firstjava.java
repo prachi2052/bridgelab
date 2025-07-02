932ac872cf28ef762535f1d6f7605393ac4dd42d
prachi2052 <127882331+prachi2052@users.noreply.github.com>
2025-07-02 12:47:30 +0530

    Add files via upload

---
+++ Firstjava.java
@@ -0,0 +1,17 @@
+public class Firstjava
+{
+public static void main(String[]args)
+{
+System.out.println("Prachi");
+}
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+String name="Prachi";
+System.out.println(name);
+
+int rollno=2052;
+System.out.println(rollno);
+
+
+}
+}
